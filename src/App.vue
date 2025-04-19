ea1ed0491473b1a278608428e901441c6db6c77b
Paul Calibuso - IG <paul.calibuso@intentgine.com>
2025-04-19 15:12:46 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+  <div class="main-container">
+    <HomePage />
+  </div>
+  
+</template>
+
+<style lang="css" scoped>
+.main-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url("./assets/home-page-background.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1 class="text-white text-6xl lg:text-8xl xl:text-8xl text-center" style="color: #8e270e;">Christian<br/>&<br/>Marjurie</h1>
+        <div class="text-white text-2xl lg:text-4xl xl:text-4xl text-center font-sans mt-6" style="color: #8e270e;">June 04, 2025 2PM Wednesday</div>
+    </div>
+    
+</template>
